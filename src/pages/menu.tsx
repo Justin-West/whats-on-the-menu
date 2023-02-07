58ff219b5d1cdb2a67ad7683c07ca257b178aeae
Justin West <39067251+Justin-West@users.noreply.github.com>
2023-02-07 14:36:08 -0800

    tweaks to food

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,7 +1,7 @@
 import items from "../food.json";
 type MenuItem = {
   name: string;
-  price: number;
+  price: string;
 };
 
 let createdMenu: Array<MenuItem> = [];
@@ -10,18 +10,50 @@ function GenMenu(f: number[]) {
   createdMenu = [];
 
   Object.keys(items.food).map((i) => {
-    let s = 0;
-    for (let j = 0; j < 5; j++) {
-      let dis = Math.abs(f[j] - items.food[i].factors[j]);
-      s += dis;
-    }
-    s *= 1 / 5;
+    let score = 0;
 
-    if (s <= 2.5)
+    //-----econ-----
+    // (+) LOWPASS, (-) HIGHPASS & PRICE
+    let priceMult = 1;
+    priceMult += f[0] / 10;
+
+    if (items.food[i].factors[0] >= 0) {
+      if (f[0] >= items.food[i].factors[0]) {
+        score += 1;
+      } //LOWPASS
+    } else {
+      if (f[0] <= Math.abs(items.food[i].factors[0])) score += 1; //HIGHPASS
+    }
+
+    //-----scale-----
+    // LOWPASS & randomly remove options
+    if (Math.random() < f[1] / 40 + 0.75) {
+      if (f[1] >= items.food[i].factors[1]) {
+        score += 1;
+      } //LOWPASS
+    }
+
+    //-----climate-----
+    //DISTANCE
+    let dist = Math.abs(f[2] - items.food[i].factors[2]);
+    if (dist < 4) score += 1;
+
+    //-----temp-----
+    //DISTANCE
+    dist = Math.abs(f[3] - items.food[i].factors[3]);
+    if (dist < 4) score += 1;
+
+    //-----water-----
+    //DISTANCE
+    dist = Math.abs(f[4] - items.food[i].factors[4]);
+    if (dist < 4) score += 1;
+
+    if (score >= 3) {
       createdMenu.push({
         name: items.food[i].name,
-        price: items.food[i].price,
+        price: (priceMult * items.food[i].price).toFixed(2),
       });
+    }
   });
   return createdMenu;
 }
