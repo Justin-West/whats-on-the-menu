a260d95228725301ff5912df30797c048cc05ddb
Justin West <39067251+Justin-West@users.noreply.github.com>
2023-02-15 13:42:12 -0800

    Added sections to menu

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -2,25 +2,48 @@
 import { useEffect, useState, Component } from "react";
 import Food from "../food.json";
 
-type MenuItem = {
+class MenuItem {
   name: string;
-  price: string;
+  price: number;
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+type MenuItemData = {
+  name: string;
+  price: number;
+  econ: number;
+  climate: number;
+  temp: number;
+  water: number;
 };
 
 export default function Menu({ f }: { f: number[] }) {
-  const [createdMenu, setMenu] = useState([{ name: "Loading...", price: "-" }]);
+  const [createdMenuMain, setMenuMain] = useState([
+    new MenuItem("Loading...", 0),
+  ]);
+  const [createdMenuSide, setMenuSide] = useState([
+    new MenuItem("Loading...", 0),
+  ]);
+  const [createdMenuDrink, setMenuDrink] = useState([
+    new MenuItem("Loading...", 0),
+  ]);
   const entreCount = 7;
   const sideCount = 10;
   const drinkCount = 5;
 
   useEffect(() => {
-    GenMenu();
+    setMenuMain(GenMenu(Food.Europe.Mains, entreCount));
+    setMenuSide(GenMenu(Food.Europe.Sides, sideCount));
+    setMenuDrink(GenMenu(Food.Europe.Drinks, drinkCount));
   }, [f]);
 
-  function GenMenu() {
-    let m: MenuItem[] = new Array();
+  function GenMenu(items: MenuItemData[], limit: number) {
+    let m = new Array<MenuItem>();
 
-    Food.Europe.Mains.map((i) => {
+    items.map((i) => {
       let score = 0;
 
       //-----econ-----
@@ -62,14 +85,11 @@ export default function Menu({ f }: { f: number[] }) {
       }
 
       if (score >= 5) {
-        m.push({
-          name: i.name,
-          price: (priceMult * i.price).toFixed(2),
-        });
+        m.push(new MenuItem(i.name, priceMult * i.price.toFixed(2)));
       }
     });
     m = RandomReduce(m, entreCount);
-    setMenu(m);
+    return m;
   }
 
   function RandomReduce(items: MenuItem[], max: number) {
@@ -92,7 +112,24 @@ export default function Menu({ f }: { f: number[] }) {
       <h1 className="mx-auto mb-2 font-extrabold text-2xl text-center border-0 border-black border-b-2">
         Menu
       </h1>
-      {createdMenu.map((i) => (
+      <p className=" text-center underline">Main Dishes</p>
+      {createdMenuMain.map((i) => (
+        <li key={i.name} className="flex">
+          <p className="grow-0">{i.name}</p>
+          <div className="dot"></div>
+          <p className="text-right grow-0">{i.price + "gp"}</p>
+        </li>
+      ))}
+      <p className=" text-center underline">Sides</p>
+      {createdMenuSide.map((i) => (
+        <li key={i.name} className="flex">
+          <p className="grow-0">{i.name}</p>
+          <div className="dot"></div>
+          <p className="text-right grow-0">{i.price + "gp"}</p>
+        </li>
+      ))}
+      <p className=" text-center underline">Drinks</p>
+      {createdMenuDrink.map((i) => (
         <li key={i.name} className="flex">
           <p className="grow-0">{i.name}</p>
           <div className="dot"></div>
